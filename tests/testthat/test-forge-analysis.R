9cb4fb5fb37d456f493000c09683ff2a1313cdc0
fubin1999 <65430559+fubin1999@users.noreply.github.com>
2025-12-13 12:31:23 +0800

    test: remove snapshot test for forge_analysis
    Snapshot test for forge_analysis is too fragile.

--- tests/testthat/test-forge-analysis.R
+++ tests/testthat/test-forge-analysis.R
@@ -1,9 +1,6 @@
 test_that("forge_analysis works", {
   # This is an integration test that checks if all parts of the system work together.
   exp <- glyexp::real_experiment2
-  expect_snapshot(
-    result <- forge_analysis(exp),
-    transform = function(x) stringr::str_replace(x, "\\[.*\\]", "[<TIME>]")
-  )
+  suppressMessages(result <- forge_analysis(exp))
   expect_s3_class(result, "glysmith_result")
 })
