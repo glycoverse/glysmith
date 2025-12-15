63c1f7a6b19255f8781942563b6c30e29bc757f5
fubin1999 <65430559+fubin1999@users.noreply.github.com>
2025-12-15 16:10:54 +0800

    feat: blueprint checks data overwrite

--- tests/testthat/test-blueprint.R
+++ tests/testthat/test-blueprint.R
@@ -5,3 +5,11 @@ test_that("blueprint checks ctx$data dependencies", {
   ))
   expect_snapshot(blueprint(step_volcano()), error = TRUE)
 })
+
+test_that("blueprint checks overwrites", {
+  steps <- list(
+    step("step1", "Step 1", function(ctx) ctx, generate = "x"),
+    step("step2", "Step 2", function(ctx) ctx, generate = "x")
+  )
+  expect_snapshot(blueprint(!!!steps))
+})
