24421d211671c4d9b5f6c80fa661271f7aa04be0
fubin1999 <65430559+fubin1999@users.noreply.github.com>
2025-12-23 17:02:16 +0800

    test: add an integration test for forge_analysis

--- tests/testthat/test-forge-analysis.R
+++ tests/testthat/test-forge-analysis.R
@@ -1,6 +1,13 @@
-test_that("forge_analysis works", {
+test_that("forge_analysis works for glycomics data", {
   # This is an integration test that checks if all parts of the system work together.
   exp <- glyexp::real_experiment2
+  suppressMessages(result <- forge_analysis(exp))
+  expect_s3_class(result, "glysmith_result")
+})
+
+test_that("forge_analysis works for glycoproteomics data", {
+  # This is an integration test that checks if all parts of the system work together.
+  exp <- glyexp::real_experiment
   suppressMessages(result <- forge_analysis(exp))
   expect_s3_class(result, "glysmith_result")
 })
