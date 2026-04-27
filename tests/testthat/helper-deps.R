7151b022f75f6744475a3b373c875ff6c47e14cf
fubin1999 <65430559+fubin1999@users.noreply.github.com>
2026-04-27 14:57:54 +0800

    revert: 51d2f0e, 5241209, 2fbd1b6
    This reverts commit 51d2f0ed18ba406cf0e1040f8adfc4b2e0baaa06, 5241209f78d89a3f7539c44aa43aeb6b40ea8939, and 2fbd1b65130df2ac1f66b204e63bcb2a99dc97c2

--- tests/testthat/helper-deps.R
+++
@@ -1,9 +0,0 @@
-skip_if_glysmith_deps_not_installed <- function() {
-  pkgs <- get_suggests_packages()
-  purrr::walk(pkgs, testthat::skip_if_not_installed)
-}
-
-forge_analysis <- function(...) {
-  skip_if_glysmith_deps_not_installed()
-  glysmith::forge_analysis(...)
-}
